dd8ce470c5981ceba182c009efa6411a18523d81
huayadlly <huayadlly@gmail.com>
2017-07-31 00:15:36 +0800

    提交；

--- src/main/java/cn/taike/action/Sample.java
+++ src/main/java/cn/taike/action/Sample.java
@@ -12,15 +12,15 @@ import static java.util.stream.Collectors.toList;
  */
 public class Sample {
 
-    public void newIterator() {
+    private static List<Dish> dishList = Arrays.asList(
+            new Dish("pizza", true, 400, Dish.Type.VEGETABLE),
+            new Dish("fish", false, 500, Dish.Type.FISH),
+            new Dish("beefs", false, 800, Dish.Type.BEEF),
+            new Dish("chickens", false, 700, Dish.Type.CHICKEN),
+            new Dish("rices", true, 700, Dish.Type.OTHER)
+    );
 
-        List<Dish> dishList = Arrays.asList(
-                new Dish("pizza", true, 400, Dish.Type.VEGETABLE),
-                new Dish("fish", false, 500, Dish.Type.FISH),
-                new Dish("beefs", false, 800, Dish.Type.BEEF),
-                new Dish("chickens", false, 700, Dish.Type.CHICKEN),
-                new Dish("rices", true, 700, Dish.Type.OTHER)
-        );
+    public static void newIterator() {
 
         //使用流的方式处理List<Dish>集合
         List<String> list = dishList.stream()
@@ -28,5 +28,11 @@ public class Sample {
                 .map(Dish::getName)
                 .limit(3)
                 .collect(toList());
+
+        System.out.println(list);
+    }
+
+    public static void main(String[] args) {
+        newIterator();
     }
 }
