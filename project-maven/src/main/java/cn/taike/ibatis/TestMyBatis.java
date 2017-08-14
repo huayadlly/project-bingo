dd5c9540fa61a85125766ff8f9f68260f49ba0a9
huayadlly <huayadlly@gmail.com>
2017-08-15 00:49:19 +0800

    ${} 与 #{};

--- project-maven/src/main/java/cn/taike/ibatis/TestMyBatis.java
+++ project-maven/src/main/java/cn/taike/ibatis/TestMyBatis.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * Created by huayadlly on 2017/8/14.
@@ -28,14 +29,40 @@ public class TestMyBatis {
 
     //根据用户id查询用户
     public static void findById() {
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        User user = sqlSession.selectOne("findUserById", 10);
-        System.out.println("user: " + user);
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = sqlSessionFactory.openSession();
+            User user = sqlSession.selectOne("findUserById", 10);
+            System.out.println("user: " + user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
 
+    //根据用户名模糊查询
+    public static void findUserByName() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = sqlSessionFactory.openSession();
+            List<User> userList = sqlSession.selectList("findUserByName", "张");
+            userList.forEach(item -> System.out.println("findByName: " + item));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
     }
 
     public static void main(String[] args) {
+
         findById();
+        findUserByName();
     }
 
 }
