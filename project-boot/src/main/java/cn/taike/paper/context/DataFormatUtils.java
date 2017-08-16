dd66748f8c3b56a8ec82895017c33ca16fded4f7
huayadlly <huayadlly@gmail.com>
2017-08-17 00:40:31 +0800

    json to Objet utils

--- project-boot/src/main/java/cn/taike/paper/context/DataFormatUtils.java
+++ project-boot/src/main/java/cn/taike/paper/context/DataFormatUtils.java
@@ -1,14 +1,27 @@
 package cn.taike.paper.context;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by huayadlly on 2017/8/16.
  */
 public class DataFormatUtils {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+    // for json
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)       // 属性为空（“”）或者为 NULL 都不序列化
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .registerModule(new JodaModule());
 
     public static String toJson(Object object) throws JsonProcessingException {
         return mapper.writeValueAsString(object);
@@ -18,8 +31,25 @@ public class DataFormatUtils {
         try {
             return mapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            return null;
         }
-        return null;
     }
+
+    public static <T> T toObjectNoException(String json, Class<T> type) {
+        try {
+            return mapper.readValue(json, type);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static List<?> toListObjectNoException(String json, Class<?> T) {
+        try {
+            JavaType javaType = mapper.getTypeFactory().constructParametricType(List.class, T);
+            return (List<?>) mapper.readValue(json, javaType);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
 }
