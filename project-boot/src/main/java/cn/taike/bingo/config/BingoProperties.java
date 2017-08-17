612b4c6b481ecbe56f2045bee5c1fb6d52f57036
huayadlly <huayadlly@gmail.com>
2017-08-17 22:56:07 +0800

    添加；

---
+++ project-boot/src/main/java/cn/taike/bingo/config/BingoProperties.java
@@ -0,0 +1,29 @@
+package cn.taike.bingo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by huayadlly on 2017/8/16.
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "paper.recognition")
+public class BingoProperties {
+
+    @NotNull
+    private String callbackUrl;
+
+    @NotNull
+    private String submitImageUrl;
+    @NotNull
+    private String evaluationCompositionUrl;
+    @NotNull
+    private String evaluationUserName;
+    @NotNull
+    private String evaluationPassword;
+
+}
